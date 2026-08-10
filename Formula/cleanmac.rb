7ad652a6c73dd1125974b5350fb9f02c8821b0de
wis <kogoome@kakao.com>
2026-08-11 00:59:02 +0900

    Update cleanmac to v1.1.1

--- Formula/cleanmac.rb
+++ Formula/cleanmac.rb
@@ -1,17 +1,17 @@
 class Cleanmac < Formula
   desc "A fast, AI-ready macOS system cleaner CLI with TUI"
   homepage "https://github.com/wis-graph/cleanmac-cli"
-  version "1.1.0"
+  version "1.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/wis-graph/cleanmac-cli/releases/download/v#{version}/cleanmac-aarch64-apple-darwin.tar.gz"
-      sha256 "f010476eec913d2a5027e5fd78b7ba999e666d63575fde1c345419b74883a125"
+      sha256 "7e870356822d8fca01646fdb8770e8ea544db59978bc8a7303cb72c672e00a79"
     end
     on_intel do
       url "https://github.com/wis-graph/cleanmac-cli/releases/download/v#{version}/cleanmac-x86_64-apple-darwin.tar.gz"
-      sha256 "3f079b56898f1478cb54c861e165479f68247c46bb1ba5517fe2c5b09edf4f4e"
+      sha256 "3ce69166bacd549ab3313aaa21553311b12ad014d7e24eaf8098dca90a97ea2f"
     end
   end
 
